f1e7f4bee483c34aef9ac832ae2a70b95365658f
a <aemail@gmail.com>
2026-02-12 00:10:26 +0530

    Fixed Next.js vulnerability

--- yourtube/.next/server/middleware-build-manifest.js
+++ yourtube/.next/server/middleware-build-manifest.js
@@ -1,40 +1,99 @@
 globalThis.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [
-    "static/chunks/react-refresh.js"
-  ],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "rootMainFilesTree": {},
   "pages": {
     "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
+      "static/chunks/fd474627a38287f0.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/595a05aeb8ced742.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/turbopack-3def603d9eda25a0.js"
     ],
     "/_app": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_app.js"
+      "static/chunks/a814f96c1a4b643f.js",
+      "static/chunks/e125dfb798dfe323.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/595a05aeb8ced742.js",
+      "static/chunks/bd4db779ecb18c5c.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/43a6e46044a54d43.js",
+      "static/chunks/328d79d4d380311f.js",
+      "static/chunks/977b591f4c3c0211.css",
+      "static/chunks/turbopack-3d3b92c4c283cdd7.js"
     ],
     "/_error": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_error.js"
+      "static/chunks/44643d74b2308b53.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/turbopack-45af91db66a1d252.js"
+    ],
+    "/channel/[id]": [
+      "static/chunks/f54f03209d79d1b5.js",
+      "static/chunks/2298a9e65a3e7952.js",
+      "static/chunks/8c78618059cdf523.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/b15d435088da0189.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/595a05aeb8ced742.js",
+      "static/chunks/turbopack-b61e99d798153997.js"
+    ],
+    "/history": [
+      "static/chunks/e5229cf9a6226e32.js",
+      "static/chunks/683d6fbd86c4ba1b.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/e125dfb798dfe323.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/519497ce24ed5820.js",
+      "static/chunks/595a05aeb8ced742.js",
+      "static/chunks/43a6e46044a54d43.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/turbopack-f809bbe8694212fb.js"
+    ],
+    "/liked": [
+      "static/chunks/6a33c23fd2a57fb0.js",
+      "static/chunks/683d6fbd86c4ba1b.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/e125dfb798dfe323.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/5881ef239a14203f.js",
+      "static/chunks/595a05aeb8ced742.js",
+      "static/chunks/43a6e46044a54d43.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/turbopack-d99a52f6850b7a9d.js"
+    ],
+    "/search": [
+      "static/chunks/48f4445f3fe479f0.js",
+      "static/chunks/8c78618059cdf523.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/turbopack-841610c58b512e66.js"
+    ],
+    "/watch-later": [
+      "static/chunks/98d19813e763e9d3.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/turbopack-4b10a0cf398ae6b3.js"
     ],
     "/watch/[id]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/watch/[id].js"
+      "static/chunks/ae0c9a163cc6aa1b.js",
+      "static/chunks/a4ef5b3f2205ef59.js",
+      "static/chunks/87003102c27f6199.js",
+      "static/chunks/4cbd103f750ba931.js",
+      "static/chunks/8412f851e4c3ffff.js",
+      "static/chunks/595a05aeb8ced742.js",
+      "static/chunks/turbopack-081086d29b187eb5.js"
     ]
   },
-  "ampFirstPages": []
+  "devFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
 ];
